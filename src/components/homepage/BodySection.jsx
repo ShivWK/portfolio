4794bf1505108cc1b9ccacb44dbc325e4e129c05
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-06 01:02:38 +0530

    add slide effect in the hero section

--- src/components/homepage/BodySection.jsx
+++ src/components/homepage/BodySection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 import {
     circleInit,
@@ -22,6 +22,7 @@ import NavigationMenu from "./NavigationMenu";
 
 const BodySection = ({ scrollOffset }) => {
     const canvasRef = useRef(null);
+    const [size, setSize] = useState(0);
 
     function particleHandler(particleArray, canvas) {
         for (let i = 0; i < particleArray.length; i++) {
@@ -30,18 +31,24 @@ const BodySection = ({ scrollOffset }) => {
         }
     }
 
+    useLayoutEffect(() => {
+        if (window.innerHeight <= 768) {
+            setSize(464);
+        } else {
+            setSize(384);
+        }
+    }, [])
+
+    console.log(size)
+
     useEffect(() => {
         if (canvasRef.current) {
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d");
 
             canvas.width = window.innerWidth;
-            canvas.height = document.documentElement.scrollHeight;
+            canvas.height = document.documentElement.scrollHeight + size;
 
-            // window.addEventListener("resize", () => {
-            //     canvas.width = window.innerWidth;
-            //     canvas.height = document.documentElement.scrollHeight;
-            // })
 
             circleInit(ctx, canvas);
             squareInit(ctx, canvas);
@@ -64,7 +71,7 @@ const BodySection = ({ scrollOffset }) => {
             }
             requestAnimationFrame(animateSquares);
         }
-    }, [])
+    }, [size])
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -78,7 +85,7 @@ const BodySection = ({ scrollOffset }) => {
 
     }, [scrollOffset])
 
-    return <section id="body" className="relative -top-20 lg:-top-24 text-gray-300 pt-18 lg:pt-24 h-full" >
+    return <section id="body" className="relative -top-[29rem] lg:-top-[24rem] animate-bodyFadeInSm lg:animate-bodyFadeInLg text-gray-300 pt-18 lg:pt-96 h-full" >
         <NavigationMenu />
 
         <canvas className="absolute top-0 left-0 bg-[linear-gradient(135deg,#000000_0%,#01111a_40%,#011d3a_70%,#021120_100%)] -z-20" ref={canvasRef}></canvas>
