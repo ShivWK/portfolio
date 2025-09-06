1da03519301bcbe2271ea5e22e7b8b8d765f37e3
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-06 18:57:03 +0530

    Refactor the code

---
+++ src/utils/data.js
@@ -0,0 +1,149 @@
+export const skillRowData = [
+    {
+        icon: "devicon:html5",
+        text: "HTML5"
+    },
+
+    {
+        icon: "devicon:css3",
+        text: "CSS3"
+    },
+
+    {
+        icon: "skill-icons:javascript",
+        text: "JavaScript"
+    },
+
+    {
+        icon: "skill-icons:typescript",
+        text: "TypeScript"
+    },
+
+    {
+        icon: "vscode-icons:file-type-reactjs",
+        text: "ReactJS"
+    },
+
+    {
+        icon: "skill-icons:redux",
+        text: "Redux"
+    },
+
+    {
+        icon: "devicon:tailwindcss",
+        text: "TailwindCSS"
+    },
+
+    {
+        icon: "devicon:bootstrap",
+        text: "BoothStrap"
+    },
+
+    {
+        icon: "devicon:reactrouter",
+        text: "React Router"
+    },
+
+    {
+        icon: "tabler:brand-framer-motion",
+        text: "Framer Motion"
+    },
+
+    {
+        icon: "devicon:materialui",
+        text: "Material UI"
+    },
+
+    {
+        icon: "logos:jwt-icon",
+        text: "JWT"
+    },
+
+    {
+        icon: "devicon:oauth",
+        text: "OAuth"
+    },
+
+    {
+        icon: "logos:firebase-icon",
+        text: "Firebase"
+    },
+
+    {
+        icon: "logos:recaptcha",
+        text: "reCaptcha"
+    },
+
+    {
+        icon: "simple-icons:reacthookform",
+        text: "React Hook Form"
+    },
+
+    {
+        icon: "logos:nodejs-icon",
+        text: "NodeJS"
+    },
+
+    {
+        icon: "skill-icons:expressjs-light",
+        text: "ExpressJS"
+    },
+
+
+    {
+        icon: "skill-icons:mongodb",
+        text: "MongoDB"
+    },
+
+
+    {
+        icon: "skill-icons:git",
+        text: "git"
+    },
+
+
+    {
+        icon: "skill-icons:github-light",
+        text: "github"
+    },
+
+    {
+        icon: "devicon:canva",
+        text: "Canva"
+    },
+
+    {
+        icon: "devicon:oauth",
+        text: "OAuth"
+    },
+
+    {
+        icon: "skill-icons:netlify-light",
+        text: "Netlify"
+    },
+
+    {
+        icon: "simple-icons:render",
+        text: "Render"
+    },
+
+    {
+        icon: "skill-icons:postman",
+        text: "Postman"
+    },
+
+    {
+        icon: "skill-icons:java-light",
+        text: "Java"
+    },
+
+    {
+        icon: "skill-icons:git",
+        text: "git"
+    },
+
+    {
+        icon: "vscode-icons:file-type-cpp3",
+        text: "C++"
+    },
+]
