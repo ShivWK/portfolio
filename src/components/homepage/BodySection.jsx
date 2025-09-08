c3a7f2076d02f4c6042c1ca1d373369e18327dbb
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-08 13:57:04 +0530

    Move canvas to the main element

--- src/components/homepage/BodySection.jsx
+++ src/components/homepage/BodySection.jsx
@@ -7,113 +7,8 @@ import Skills from "./about/Skills";
 import GithubStats from "./about/GithubStats";
 import Projects from "./projects/Projects";
 
-import {
-    circleInit,
-    squareInit,
-    triangleInit,
-    hexagonInit,
-    pentagonInit,
-    diamondInit
-} from "../../utils/initializer";
-
-import {
-    circleParticlesArray,
-    squareParticlesArray,
-    triangleParticlesArray,
-    pentagonParticlesArray,
-    hexagonParticlesArray,
-    diamondParticlesArray
-} from "../../utils/initializer";
-
-const BodySection = ({ scrollOffset }) => {
-    const [extraSize, setExtraSize] = useState(0);
-    const [ready, setReady] = useState(false);
-    const [size, setSize] = useState("");
-    const canvasRef = useRef(null);
-
-    function particleHandler(particleArray, canvas) {
-        for (let i = 0; i < particleArray.length; i++) {
-            particleArray[i].update(scrollOffset, canvas);
-            particleArray[i].draw();
-        }
-    }
-
-    useEffect(() => {
-        const resizeHandler = () => {
-            if (window.innerWidth <= 768) {
-                setSize("small");
-                setExtraSize(64);
-
-            } else {
-                setSize("large");
-                setExtraSize(80);
-            }
-        }
-        resizeHandler();
-
-        window.addEventListener("resize", resizeHandler);
-        return () => window.removeEventListener("resize", resizeHandler);
-    }, []);
-
-    useEffect(() => {
-        const startCanvas = () => {
-            if (canvasRef.current) {
-                const canvas = canvasRef.current;
-                const ctx = canvas.getContext("2d");
-
-                canvas.width = window.innerWidth;
-                canvas.height = document.getElementById("body").scrollHeight;
-
-                circleInit(ctx, canvas);
-                squareInit(ctx, canvas);
-                triangleInit(ctx, canvas);
-                pentagonInit(ctx, canvas);
-                hexagonInit(ctx, canvas);
-                diamondInit(ctx, canvas);
-
-                function animateSquares() {
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                    particleHandler(pentagonParticlesArray, canvas);
-                    particleHandler(hexagonParticlesArray, canvas)
-                    particleHandler(squareParticlesArray, canvas);
-                    particleHandler(circleParticlesArray, canvas);
-                    particleHandler(triangleParticlesArray, canvas);
-                    particleHandler(diamondParticlesArray, canvas);
-
-                    requestAnimationFrame(animateSquares);
-                }
-                requestAnimationFrame(animateSquares);
-
-                const resizeHandler = () => {
-                    canvas.height = document.getElementById("body").scrollHeight;
-                    canvas.width = window.width;
-                }
-
-                document.getElementById("body").addEventListener("resize", resizeHandler);
-                return () => window.removeEventListener("resize", resizeHandler);
-            }
-        }
-
-        const schedule = window.requestIdleCallback || ((cb) => setTimeout(cd, 200));
-        schedule(() => {
-            startCanvas();
-            setReady(true);
-        }, { timeout: 1000 })
-
-    }, [])
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-
-        particleHandler(pentagonParticlesArray, canvas);
-        particleHandler(hexagonParticlesArray, canvas);
-        particleHandler(squareParticlesArray, canvas);
-        particleHandler(circleParticlesArray, canvas);
-        particleHandler(triangleParticlesArray, canvas);
-        particleHandler(diamondParticlesArray, canvas);
-
-    }, [scrollOffset]); //border border-white
+const BodySection = ({ scrollOffset, size }) => {
+     //border border-white
 
     return <section id="body" className="relative flex flex-col gap-14 lg:gap-24 -top-16 lg:-top-20 text-gray-300 " >
         <NavigationMenu />
@@ -129,8 +24,6 @@ const BodySection = ({ scrollOffset }) => {
             </Suspense>
         </div>
         <Projects />
-
-        <canvas ref={canvasRef} className={`absolute top-0 left-0 bg-transparent ${ready && "animate-canvasFadeIn"} -z-20`}></canvas>
     </section>
 }
 
--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -1,9 +1,30 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import HeroSection from "./HeroSection";
 import BodySection from "./BodySection";
 
+import {
+    circleInit,
+    squareInit,
+    triangleInit,
+    hexagonInit,
+    pentagonInit,
+    diamondInit
+} from "../../utils/initializer";
+
+import {
+    circleParticlesArray,
+    squareParticlesArray,
+    triangleParticlesArray,
+    pentagonParticlesArray,
+    hexagonParticlesArray,
+    diamondParticlesArray
+} from "../../utils/initializer";
+
 const Home = () => {
     const [scrollOffset, setScrollOffset] = useState(0);
+    const [ready, setReady] = useState(false);
+    const [size, setSize] = useState("");
+    const canvasRef = useRef(null);
     const lastClientHeight = useRef(0);
 
     useEffect(() => {
@@ -22,9 +43,92 @@ const Home = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+    function particleHandler(particleArray, canvas) {
+        for (let i = 0; i < particleArray.length; i++) {
+            particleArray[i].update(scrollOffset, canvas);
+            particleArray[i].draw();
+        }
+    }
+
+    useEffect(() => {
+        const resizeHandler = () => {
+            if (window.innerWidth <= 768) {
+                setSize("small");
+            } else {
+                setSize("large");
+            }
+        }
+        resizeHandler();
+
+        window.addEventListener("resize", resizeHandler);
+        return () => window.removeEventListener("resize", resizeHandler);
+    }, []);
+
+    useEffect(() => {
+        const startCanvas = () => {
+            if (canvasRef.current) {
+                const canvas = canvasRef.current;
+                const ctx = canvas.getContext("2d");
+
+                canvas.width = window.innerWidth;
+                canvas.height = document.getElementById("main").scrollHeight;
+
+                circleInit(ctx, canvas);
+                squareInit(ctx, canvas);
+                triangleInit(ctx, canvas);
+                pentagonInit(ctx, canvas);
+                hexagonInit(ctx, canvas);
+                diamondInit(ctx, canvas);
+
+                function animateSquares() {
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                    particleHandler(pentagonParticlesArray, canvas);
+                    particleHandler(hexagonParticlesArray, canvas)
+                    particleHandler(squareParticlesArray, canvas);
+                    particleHandler(circleParticlesArray, canvas);
+                    particleHandler(triangleParticlesArray, canvas);
+                    particleHandler(diamondParticlesArray, canvas);
+
+                    requestAnimationFrame(animateSquares);
+                }
+                requestAnimationFrame(animateSquares);
+
+                const resizeHandler = () => {
+                    canvas.height = document.getElementById("body").scrollHeight;
+                    canvas.width = window.width;
+                }
+
+                document.getElementById("body").addEventListener("resize", resizeHandler);
+                return () => window.removeEventListener("resize", resizeHandler);
+            }
+        }
+
+        const schedule = window.requestIdleCallback || ((cb) => setTimeout(cd, 200));
+        schedule(() => {
+            startCanvas();
+            setReady(true);
+        }, { timeout: 1000 })
+
+    }, [])
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        particleHandler(pentagonParticlesArray, canvas);
+        particleHandler(hexagonParticlesArray, canvas);
+        particleHandler(squareParticlesArray, canvas);
+        particleHandler(circleParticlesArray, canvas);
+        particleHandler(triangleParticlesArray, canvas);
+        particleHandler(diamondParticlesArray, canvas);
+
+    }, [scrollOffset]);
+
     return <main id="main" className="relative bg-[linear-gradient(135deg,#000000_0%,#01111a_40%,#011d3a_70%,#021120_100%)] -z-10">
-        <HeroSection scrollOffset={scrollOffset} />
-        <BodySection scrollOffset={scrollOffset} />
+        <HeroSection scrollOffset={scrollOffset} size={size} />
+        <BodySection scrollOffset={scrollOffset} size={size} />
+
+        <canvas ref={canvasRef} className={`absolute top-0 left-0 bg-transparent ${ready && "animate-canvasFadeIn"} -z-20`}></canvas>
     </main>
 }
 
