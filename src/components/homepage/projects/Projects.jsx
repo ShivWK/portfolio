0f9a244af632bee1ff91ae5f591da660defb3978
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-09 01:02:21 +0530

    Add badges and description

--- src/components/homepage/projects/Projects.jsx
+++ src/components/homepage/projects/Projects.jsx
@@ -7,13 +7,15 @@ const Projects = ({ size }) => {
     const [ready, setReady] = useState(false);
     const containerRef = useRef();
 
+    const badges = ["FullStack", "API Integration", "Authentication"]
+
     useIntersection(containerRef, setReady);
 
     return <section id="projects" ref={containerRef} className={`mx-auto lg:w-[1024px] max-lg:px-3 transform ${ready ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"} transition-all duration-400 ease-linear scroll-mt-30`}>
-        <h2 className="text-3xl lg:text-4xl w-fit font-semibold font-heading tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 mb-4 lg:mb-8 self-start">Projects</h2>
+        <h2 className="text-3xl lg:text-4xl w-fit font-semibold font-heading tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 mb-6 lg:mb-9 self-start">Projects</h2>
 
         <div className="flex flex-col lg:flex-row lg:gap-8 gap-4 w-full lg:h-[30rem]" >
-            <div className="flex flex-col items-center gap-2 lg:basis-[48%] basis-full backdrop-blur-md rounded-xl shadow-[0_0_15px_2px_rgba(3,165,252,0.6)]">
+            <div className="flex flex-col items-center gap-0.5 lg:gap-1 lg:basis-[48%] basis-full backdrop-blur-md rounded-xl shadow-[0_0_15px_2px_rgba(3,165,252,0.6)]">
                 <div id="projectPic" className="relative group h-52 lg:h-72 overflow-hidden border-4 border-[#011932] rounded-xl">
                     <img src="/images/zestyeats.png" alt="Project home screen" className="object-cover w-full h-full  hover:scale-110 transition-all duration-300 ease-in-out" />
                     <a href="https://zestyeats.shivendra.site/" target="__blank" className="group projectTag -top-1 -left-1 tracking-wider rounded-br-xl flex items-center gap-2 lg:text-lg font-medium lg:font-semibold px-2 lg:px-3 py-0.5">
@@ -25,8 +27,10 @@ const Projects = ({ size }) => {
                         <Icon icon="skill-icons:github-light" width={size === "small" ? 18 : 20} height={size === "small" ? 18 : 20} className="group-hover:animate-pulse" />
                     </a>
                 </div>
-                <div id="projectInfo" className="w-full px-1 flex flex-col gap-0.5 lg:gap-2">
-                    <p id="title" className="text-xl lg:text-2xl font-bold font-heading tracking-wide bg-gradient-to-r from-blue-200 to-cyan-300 bg-clip-text text-transparent leading-6">ZestyEats – Online Food Delivery App</p>
+                <div id="projectInfo" className="w-full p-1 flex flex-col gap-1 lg:gap-4">
+                    <p id="title" className="text-xl lg:text-2xl font-bold font-heading tracking-wide bg-gradient-to-r from-blue-200 to-cyan-300 bg-clip-text text-transparent leading-6">
+                        ZestyEats – Online Food Delivery App
+                    </p>
                     <div className="flex items-center gap-1.5 lg:gap-3 flex-wrap">
                         <Icon icon="vscode-icons:file-type-reactjs" width={size === "small" ? 25 : 30} height={size === "small" ? 25 : 30} className="group-hover:animate-pulse" />
                         <Icon icon="skill-icons:javascript" width={size === "small" ? 25 : 30} height={size === "small" ? 25 : 30} className="group-hover:animate-pulse" />
@@ -40,6 +44,13 @@ const Projects = ({ size }) => {
                         <Icon icon="logos:recaptcha" width={size === "small" ? 25 : 30} height={size === "small" ? 25 : 30} className="group-hover:animate-pulse" />
                         <Icon icon="simple-icons:razorpay" width={size === "small" ? 25 : 30} height={size === "small" ? 25 : 30} className="group-hover:animate-pulse" />
                     </div>
+                    <div className="flex flex-wrap items-center gap-1 lg:gap-2 self-start">
+                        {badges.map(data => <div className="px-1.5 py-0.5 lg:px-3 lg:py-1 rounded-md bg-[#d13b21]">
+                            <p className="tracking-wider font-semibold text-white">{data}</p>
+                        </div>)}
+                    </div>
+
+                    <p className="text-justify font-medium leading-5">A full-stack food delivery app inspired by Swiggy, featuring location-based restaurant discovery, secure authentication, and seamless online payments.</p>
                 </div>
             </div>
         </div>
