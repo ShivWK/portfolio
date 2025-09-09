449807918c9e6c8a0250c90fb3882dd1c9da14d9
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-09 18:10:15 +0530

    print project cards dynamically

--- src/components/homepage/projects/Projects.jsx
+++ src/components/homepage/projects/Projects.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react"
 import useIntersection from "../../../utils/IntersectionObserver"
 import { CircleArrowRight, Cog } from "lucide-react";
 import { Icon } from "@iconify/react";
+import { majorProjects } from "../../../utils/projectsData";
 
 const Projects = ({ size }) => {
     const [ready, setReady] = useState(false);
@@ -15,7 +16,56 @@ const Projects = ({ size }) => {
         <h2 className="text-3xl lg:text-4xl w-fit font-semibold font-heading tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300 mb-6 lg:mb-9 self-start">Projects</h2>
 
         <div className="flex flex-col lg:flex-row lg:gap-8 gap-4 w-full" >
-            <div className="flex flex-col items-center gap-0.5 lg:gap-1 lg:basis-[48%] basis-full backdrop-blur-md rounded-xl shadow-[0_0_15px_2px_rgba(3,165,252,0.6)]">
+            {
+                majorProjects.map((data, index) => {
+                    return <div key={index} className="flex flex-col items-center gap-0.5 lg:gap-1 lg:basis-[48%] basis-full backdrop-blur-md rounded-xl shadow-[0_0_15px_2px_rgba(3,165,252,0.6)]">
+                        <div id="projectPic" className="relative group h-52 lg:h-72 overflow-hidden border-4 border-[#011932] rounded-xl">
+                            <img src={data.homeScreenPic} alt="Project home screen" className="object-cover w-full h-full  hover:scale-110 transition-all duration-300 ease-in-out" />
+                            <a href={data.liveLink} target="__blank" className="group projectTag -top-1 -left-1 tracking-wider rounded-br-xl flex items-center gap-1.5 lg:gap-2 lg:text-lg font-medium lg:font-semibold px-2 lg:px-3 py-0.5">
+                                <p>{data.underDevelopment ? "Live Demo" : "Live"}</p>
+                                <CircleArrowRight size={size === "small" ? 18 : 20} strokeWidth={2.5} className="transform rotate-[-45deg] group-hover:animate-pulse" />
+                            </a>
+                            <a href={data.githubLink} target="__blank" className="group projectTag z-10 tracking-wider -bottom-1 -right-1 rounded-tl-xl flex items-center gap-1.5 lg:gap-2 lg:text-lg font-medium lg:font-semibold px-2 lg:px-3 py-0.5">
+                                <p>github </p>
+                                <Icon icon="skill-icons:github-light" width={size === "small" ? 18 : 20} height={size === "small" ? 18 : 20} className="group-hover:animate-pulse" />
+                            </a>
+                            {
+                                data.underDevelopment && <div className="absolute left-1 bottom-1 flex items-center gap-1.5 lg:gap-2 px-1.5 lg:px-2 py-0.5 rounded-md bg-[linear-gradient(to_right,#2575FC,#6A11CB,#667EEA)]">
+                                    <Cog size={size === "small" ? 18 : 20} strokeWidth={2.5} className="animate-spin text-white" />
+                                    <p className="text-white max-lg:text-sm tracking-wide font-heading font-medium lg:font-semibold z-20">Under Development</p>
+                                </div>
+                            }
+                        </div>
+                        <div id="projectInfo" className="w-full p-2 flex flex-col gap-2 lg:gap-4">
+                            <p id="title" className="text-xl lg:text-2xl font-bold font-heading tracking-wide bg-gradient-to-r from-blue-200 to-cyan-300 bg-clip-text text-transparent leading-6">
+                                {data.title}
+                            </p>
+                            <div className="flex items-center gap-1.5 lg:gap-3 flex-wrap">
+                                {
+                                data.techStack.map((data, index) => <Icon key={index} icon={data.icon} width={size === "small" ? 25 : 30} height={size === "small" ? 25 : 30} className="group-hover:animate-pulse" />)
+                                }
+                            </div>
+                            <div className="flex flex-wrap items-center gap-1 lg:gap-2 self-start">
+                                {data.badges.map((data, index) => <div key={index} className="px-1.5 py-0.5 lg:px-3 lg:py-1 rounded-md bg-[#9f0712]">
+                                    <p className="tracking-wider font-semibold text-white">{data}</p>
+                                </div>)}
+                            </div>
+
+                            <p className="text-justify font-medium leading-5 text-lg">
+                                {data.description}
+                            </p>
+
+                            <ul className="list-disc pl-4 lg:pl-5 mt-1 marker:text-blue-400">
+                                {data.featuredHeighLights.map((data, index) => {
+                                    return <li key={index} className="" dangerouslySetInnerHTML={{__html: data}}></li>
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                })
+            }
+            {/* Project card */}
+            {/* <div className="flex flex-col items-center gap-0.5 lg:gap-1 lg:basis-[48%] basis-full backdrop-blur-md rounded-xl shadow-[0_0_15px_2px_rgba(3,165,252,0.6)]">
                 <div id="projectPic" className="relative group h-52 lg:h-72 overflow-hidden border-4 border-[#011932] rounded-xl">
                     <img src="/images/zestyeats.png" alt="Project home screen" className="object-cover w-full h-full  hover:scale-110 transition-all duration-300 ease-in-out" />
                     <a href="https://zestyeats.shivendra.site/" target="__blank" className="group projectTag -top-1 -left-1 tracking-wider rounded-br-xl flex items-center gap-1.5 lg:gap-2 lg:text-lg font-medium lg:font-semibold px-2 lg:px-3 py-0.5">
@@ -60,15 +110,10 @@ const Projects = ({ size }) => {
 
                     <ul className="">
                         <li className="text-justify">Seamless Payments – Integrated Razorpay for secure and fast transactions.</li>
-                        <li className="text-justify">Robust Authentication – Supports OTP login (phone + email) and OAuth 2.0 (Google email login).</li>
-                        <li className="text-justify">Smart Location Services – Used Geocoding API to fetch user’s real-time location.</li>
-                        <li className="text-justify">Dynamic Restaurant Data – Fetches menus & restaurants for multiple cities and localities across India.</li>
-                        <li className="text-justify">Proxy Server for APIs – Implemented secure API proxy server.</li>
-                        <li className="text-justify">Optimized Performance – Implemented debouncing, lazy loading, infinite scroll, shimmer UI for smooth UX.</li>
                     </ul>
-
                 </div>
-            </div>
+            </div> */}
+
         </div>
     </section>
 }
---
+++ src/utils/projectsData.js
@@ -0,0 +1,69 @@
+export const majorProjects = [
+    {
+        homeScreenPic: "/images/zestyeats.png",
+        liveLink: "https://zestyeats.shivendra.site/",
+        githubLink: "https://github.com/ShivWK/ZestyEats.git",
+        underDevelopment: false,
+        title: "ZestyEats – Online Food Delivery App",
+        techStack: [
+            {
+                icon: "vscode-icons:file-type-reactjs",
+                text: "ReactJS"
+            },
+
+            {
+                icon: "skill-icons:javascript",
+                text: "JavaScript"
+            },
+
+            {
+                icon: "devicon:tailwindcss",
+                text: "TailwindCSS"
+            },
+
+            {
+                icon: "devicon:reactrouter",
+                text: "React Router"
+            },
+
+            {
+                icon: "skill-icons:redux",
+                text: "React Redux"
+            },
+
+            {
+                icon: "logos:nodejs-icon",
+                text: "NodeJS"
+            },
+
+            {
+                icon: "skill-icons:expressjs-light",
+                text: "ExpressJS"
+            },
+            {
+                icon: "skill-icons:mongodb",
+                text: "MongoDB"
+            },
+            {
+                icon: "devicon-plain:oauth",
+                text: "OAuth 2.0"
+            },
+            {
+                icon: "logos:recaptcha",
+                text: "Google reCAPTCHA"
+            },
+
+        ],
+
+        badges: ["FullStack", "API Integration", "Authentication", "Payment Integration"],
+        description: " A full-stack food delivery app inspired by Swiggy, featuring location-based restaurant discovery, secure authentication, and seamless online payments.",
+        featuredHeighLights: [
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Seamless Payments:</strong> Integrated <span style='font-weight: 500; color: #53eafd'>Razorpay</span> for secure and fast transactions.",
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Robust Authentication:</strong> Supports <span style='font-weight: 500; color: #53eafd'>OTP login (phone + email)</span> and <span style='font-weight: 500; color: #53eafd'>OAuth 2.0</span> (Google email login).",
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Smart Location Services:</strong> Used <span style='font-weight: 500; color: #53eafd'>Geocoding API</span> to fetch user’s real-time location.",
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Dynamic Restaurant Data:</strong> Fetches menus & restaurants for <span style='font-weight: 500; color: #53eafd'>multiple cities</span> and localities <span style='font-weight: 500; color: #53eafd'>across India.</span>",
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Proxy Server for APIs:</strong> Implemented secure API proxy server.",
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Optimized Performance:</strong> Implemented <span style='font-weight: 500; color: #53eafd'>debouncing</span>, <span style='font-weight: 500; color: #53eafd'>lazy loading</span>, <span style='font-weight: 500; color: #53eafd'>infinite scroll</span>, <span style='font-weight: 500; color: #53eafd'>shimmer UI</span> for smooth UX."
+        ]
+    }
+]
