abc5531f8f0255258545bd5294e697a80e76b128
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-09-15 23:00:00 +0530

    Add testimonials

--- src/components/tutorials/Tutorials.jsx
+++ src/components/tutorials/Tutorials.jsx
@@ -5,7 +5,7 @@ const Tutorials = () => {
         <div className="relative rounded-xl overflow-hidden lg:w-[40%] w-full h-[27rem] lg:h-[31rem]">
             <img src="/tutorials/Axios.png" alt="Axios VS Fetch tutorial" className="h-full w-full cursor-pointer" />
             <div className="absolute bottom-0 left-0 w-full bg-black/80 z-40 rounded-tl-xl p-3">
-                <div className="absolute -top-8 right-0 px-2 py-1 font-medium tracking-wide bg-black/80 rounded-t-xl flex items-center gap-2">
+                <div className="absolute -top-8 right-0 px-2 py-1 font-medium tracking-wide bg-black/80 rounded-t-xl flex items-center gap-2 max-md:text-sm">
                     <Heart fill="red" className="text-red-600" />
                     <span>400</span>
                     <Repeat className="text-[#53eafd]" />
