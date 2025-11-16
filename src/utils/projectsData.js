961b458dde0c0d1d73fc6d327143e431249d680d
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-11-16 13:17:58 +0530

    Add new major project

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -82,6 +82,85 @@ export const majorProjects = [
             "<strong style='color: oklch(70.7% 0.165 254.624);'>Optimized Performance:</strong> Implemented <span style='font-weight: 500; color: #53eafd'>debouncing</span>, <span style='font-weight: 500; color: #53eafd'>lazy loading</span>, <span style='font-weight: 500; color: #53eafd'>infinite scroll</span>, <span style='font-weight: 500; color: #53eafd'>shimmer UI</span> for smooth UX."
         ]
     },
+
+    {
+        homeScreenPic: "/images/promptTube1.png",
+        linkAvailable: true,
+        liveLink: "https://prompttube-ai.shivendra.site",
+        alternateLinkText: "Coming Soon",
+        githubLink: "https://github.com/ShivWK/promptTube.git",
+        underDevelopment: true,
+        title: "PromptTube – AI-Powered YouTube Video Discovery App",
+        techStack: [
+            {
+                icon: "vscode-icons:file-type-reactjs",
+                text: "ReactJS"
+            },
+
+            {
+                icon: "skill-icons:javascript",
+                text: "JavaScript"
+            },
+
+            {
+                icon: "devicon:tailwindcss",
+                text: "TailwindCSS"
+            },
+
+            {
+                icon: "devicon:reactrouter",
+                text: "React Router"
+            },
+
+            {
+                icon: "skill-icons:redux",
+                text: "React Redux"
+            },
+
+            {
+                icon: "logos:nodejs-icon",
+                text: "NodeJS"
+            },
+
+            {
+                icon: "skill-icons:expressjs-light",
+                text: "ExpressJS"
+            },
+
+            {
+                icon: "skill-icons:mongodb",
+                text: "MongoDB"
+            },
+
+            {
+                icon: "logos:firebase-icon",
+                text: "Firebase Auth"
+            },
+
+            {
+                icon: "vscode-icons:file-type-gemini",
+                text: "Google Gemini"
+            },
+        ],
+
+        badges: ["FullStack", "API Integration", "Authentication"],
+
+        description: "A full-stack food delivery app inspired by Swiggy, featuring location-based restaurant discovery, secure authentication, and seamless online payments.",
+
+        featuredHeighLights: [
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Seamless Payments:</strong> Integrated <span style='font-weight: 500; color: #53eafd'>Razorpay</span> for secure and fast transactions.",
+
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Robust Authentication:</strong> Supports <span style='font-weight: 500; color: #53eafd'>OTP login (phone + email)</span> and <span style='font-weight: 500; color: #53eafd'>OAuth 2.0</span> (Google email login).",
+
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Smart Location Services:</strong> Used <span style='font-weight: 500; color: #53eafd'>Geolocation and Geocoding API</span> to fetch user’s real-time location.",
+
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Dynamic Restaurant Data:</strong> Fetches menus & restaurants for <span style='font-weight: 500; color: #53eafd'>multiple cities</span> and localities <span style='font-weight: 500; color: #53eafd'>across India.</span>",
+
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Proxy Server for APIs:</strong> Implemented secure API proxy server.",
+
+            "<strong style='color: oklch(70.7% 0.165 254.624);'>Optimized Performance:</strong> Implemented <span style='font-weight: 500; color: #53eafd'>debouncing</span>, <span style='font-weight: 500; color: #53eafd'>lazy loading</span>, <span style='font-weight: 500; color: #53eafd'>infinite scroll</span>, <span style='font-weight: 500; color: #53eafd'>shimmer UI</span> for smooth UX."
+        ]
+    },
 ]
 
 export const minProjects = [
